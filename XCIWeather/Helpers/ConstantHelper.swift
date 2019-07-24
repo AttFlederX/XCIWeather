bfb84c80c9fcfd851e660fc27600f53973c8645d
Dmitry Afanasiev <dmitry.afanasiev@jetsoftpro.com>
2019-07-24 12:26:11 +0300

    - Remove API key

--- XCIWeather/Helpers/ConstantHelper.swift
+++ XCIWeather/Helpers/ConstantHelper.swift
@@ -12,5 +12,5 @@ class ConstantHelper {
     static let baseWeatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%@&"
     static let baseWeatherApiForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=%f&lon=%f&appid=%@&"
 
-    static let apiKey = "8d16dc5b3caa5af6a15c0f120731ffad"
+    static let apiKey = "CHANGEME"
 }
